778727e5b80b5c536e510c94fe3889d3fc80d28c
KDaeam <eodka1234@gmail.com>
2026-05-26 20:16:07 +0900

    fix: remove VS files and update gitignore

---
+++ Text_RPG_Team11/Text_RPG_Team11/LogManager.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include "LogManager.h"
+
+using namespace std;
+
+LogManager& LogManager::getInstance() {
+	static LogManager instance;
+	return instance;
+}
+
+void LogManager::addLog(const string& message) {
+	eventLog.push_back(message);
+}
+
+void LogManager::recordKill(const string& monsterName) {
+	mosterKillCounts[monsterName]++;
+	addLog("[" + monsterName + "]을(를) 처치했습니다");
+}
+
+void LogManager::showLogs() const {
+	cout << "\n========================================\n";
+	cout << "               [ 게임 로그 ]            \n";
+	cout << "========================================\n";
+	if (eventLog.empty())
+	{
+		cout << "기록된 로그가 없습니다\n";
+	}
+	for (const auto& log : eventLog)
+	{
+		cout << "▶ " << log << "\n";
+	}
+	cout << "========================================\n";
+}
+
+void LogManager::showKillCounts() const {
+	cout << "\n========================================\n";
+	cout << "           [ 처치한 몬스터 통계 ]         \n";
+	cout << "========================================\n";
+	if (mosterKillCounts.empty())
+	{
+		cout << "아직 처치한 몬스터가 없습니다\n";
+	}
+	for (const auto& pair : mosterKillCounts) {
+		cout << " 💀 " << pair.first << " : 총 " << pair.second << " 마리\n";
+	}
+	cout << "========================================\n";
+
+}
