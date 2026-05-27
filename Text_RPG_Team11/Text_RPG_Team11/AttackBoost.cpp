8874ee26632670affd914a995962b975c5654b54
KDaeam <eodka1234@gmail.com>
2026-05-27 11:54:04 +0900

    아이템
    아이템 관련 파일 전부 프로젝트 파일로 옮기고 다시 푸시합니당

---
+++ Text_RPG_Team11/Text_RPG_Team11/AttackBoost.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include "AttackBoost.h"
+
+
+class Character;
+
+
+
+AttackBoost::AttackBoost() :Item("공격력 증가 알약", "알약", "공격력 증가", 5)
+{
+
+	
+
+}
+
+void AttackBoost::use(Character* character)
+{
+	//사용했을 때 일어나는 효과 일어날 함수
+	
+	
+		int PlayerAtk;
+		PlayerAtk = character->getatk() + increaseAmount;
+
+		character->setAtk(PlayerAtk);
+		
+	
+
+	
+
+
+}
+
+void AttackBoost::removeEffectAfterBattle(Character* character)
+{
+	//공격력 증가 버프 제거 함수, 전투 종료 시 한 번 실행해주세요
+	int PlayerAtk;
+	PlayerAtk = character->getatk() - increaseAmount;
+	character->setAtk(PlayerAtk);
+}
+
+
+
+
